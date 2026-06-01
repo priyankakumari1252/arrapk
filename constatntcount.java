3e3102847b2743e6bcc8a7b0620f38a825fc7ca1
priyankakumari1252 <priyanka05652030@gmail.com>
2026-06-02 01:39:08 +0530

    Add files via upload

---
+++ constatntcount.java
@@ -0,0 +1,26 @@
+import java.util.*;
+public class constatntcount {
+
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		int i;
+		
+		System.out.println("enter any string");
+		String s=sc.nextLine();
+		
+		int count=0;
+		
+		
+		for(i=0; i<s.length(); i++)
+		{
+			char ch= s.charAt(i);
+			if(ch!='A'&& ch!='a'&& ch!='E'&& ch!='e'&& ch!='I'&& ch!='i'&& ch!='O'&& ch!='o'&& ch!='U'&&  ch!='u')
+			{
+				count++;
+			}
+		}
+		System.out.println("Consonant are:" + count);
+		sc.close();
+	}
+
+}
